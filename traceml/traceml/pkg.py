5962f483b8e03823594cb86fde31678c4d5db2f5
polyaxon-ci <ci@polyaxon.com>
2023-01-16 02:02:21 +0100

    Bump version to v1.0.6

--- traceml/traceml/pkg.py
+++ traceml/traceml/pkg.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 NAME = "traceml"
-VERSION = "1.0.5"
+VERSION = "1.0.6"
 DESC = (
     "Engine for ML/Data tracking, visualization, dashboards, and model UI for Polyaxon."
 )
