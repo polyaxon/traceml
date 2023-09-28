23e3fde5a484bba3b437db9e2a72cef0a2ef791f
polyaxon-ci <ci@polyaxon.com>
2023-09-28 17:02:17 +0200

    Make py-3.8 minimum supported version

--- traceml/setup.py
+++ traceml/setup.py
@@ -91,7 +91,7 @@ setup(
     ],
     install_requires=[],
     extras_require=extra,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "Intended Audience :: Information Technology",
         "Intended Audience :: System Administrators",
