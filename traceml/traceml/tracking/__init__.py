c13b79bcab925f76a26ed2bb08054f1d4b88d190
mouradmourafiq <mouradmourafiq@gmail.com>
2026-08-27 12:19:09 +0200

    Update schemas import

--- traceml/traceml/tracking/__init__.py
+++ traceml/traceml/tracking/__init__.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import Any, Dict, List, Optional, Sequence, Union
 
 from polyaxon._client.run import RunClient
-from polyaxon._sdk.schemas import V1Run
+from polyaxon._sdk.schemas.v1_run import V1Run
 from traceml.artifacts import V1ArtifactKind, V1RunArtifact
 from traceml.tracking.run import Run
 
